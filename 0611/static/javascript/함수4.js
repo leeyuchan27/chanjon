32d74ee064a056b1dc06e7abbea6b24caffda158
leeyuchan27 <dltmdals574@naver.com>
2024-06-11 17:25:57 +0900

    0611

---
+++ 0611/static/javascript/함수4.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+  //브라우저에 모두 표시되면 발생하는 이벤트 onload
+  
+  document.getElementById("calc").addEventListener('click',function(){
+
+  });
+}
+
+function 총점계산(a,b,c,d){//총점 계산 만 하는 함수
+var total= ;
+}
+function 평균계산(total){//평균계산만하는함수
+    var avg=;
+}
+function 출력(){//총점과 평균을 화면에 출력하는 함수
+
+}
